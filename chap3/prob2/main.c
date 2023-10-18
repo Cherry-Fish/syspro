ddbdfd5faad6a97ef5dd8f1dc3d4c4fa58c7f92d
Cherry-Fish <acvse@cu.ac.kr>
2023-10-18 15:05:51 +0900

    어우;

--- chap3/prob2/main.c
+++ chap3/prob2/main.c
@@ -2,34 +2,32 @@
 #include <string.h>
 #include "copy.h"
 
-char line[MAXLINE]; // 입력 줄
-int llen[5];
-main(){
-	int len;
-	int max;
-	max = 0;
-	cnt=0;
-	int temp;
-	while(cnt<5) {
-		gets(line); //문자열 입력
-    		len = strlen(line); //문자열의 길이
-		llen[cnt]=len;
-		cnt++;
-	}
-	for(i=0;i<5;i++){
-		for(j=o;j<5;j++){
-			if(llen[i]>llen[j]){
-				temp=line[i];
-				line[i]=line[j];
-				line[j]=temp;
-			}
-		}
-	}
-				
-	/*if(max > 0) // 입력 줄이 있었다면
-		printf("%s \n", longest);
-	return 0;*/
-	for(i=0;i<5;i++){
-	printf("%c\n",line[i]);
-	}
+int main() {
+
+    char str[MAXLINE][MAXLINE];
+    char longest[MAXLINE]; 
+    int i, j;
+    int cnt=0;
+    
+    while(cnt<5){
+        scanf(" %s", str[cnt]);
+        cnt++;
+    }
+
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4 - i; j++) {
+            if (strlen(str[j]) < strlen(str[j + 1])) {
+                copy(str[j], longest);
+                copy(str[j+1], str[j]);
+                copy(longest, str[j+1]);
+                
+            }
+        }
+    }
+    for (i = 0; i < 5; i++) {
+        printf("%s\n", str[i]);
+    }
+
+    return 0;
 }
+
